d2f544c2658f0bb9f28a5e475b3cb1906e1b4000
Travis Masingale <4572430+sicxz@users.noreply.github.com>
2026-01-08 00:42:05 +0000

    Add placeholder content and alert in index.html and main.js for initial setup

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,11 @@
 
 console.log('Portfolio loaded');
 
+// Week 0: Hello World!
+alert("Hello World!" );
+
 // Week 1: You'll add theme toggle code here
 // Week 2: You'll add localStorage persistence here
 // Week 3+: More to come...
+
+
